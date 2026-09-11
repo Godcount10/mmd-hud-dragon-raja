6e6f4f8b41f1edc382c90a9310dc17117f7df7b2
Godcount10 <1523219794@qq.com>
2026-09-11 10:41:54 +0800

    fix(dragon-raja): configure external poster and brush font URLs

--- vite.frame.config.ts
+++ vite.frame.config.ts
@@ -5,9 +5,13 @@ import { frameCssInjectionPlugin } from './build/frameCssInjection'
 
 const packageJson = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf8')) as { version: string }
 const frameOutDir = process.env.MMD_HUD_FRAME_OUT_DIR ?? 'dist/frame'
+const DEFAULT_DRAGON_RAJA_WELCOME_POSTER_URL =
+  'https://meimoaiimg.com/user/701473/image-1787581608135.jpg?i=1344574'
+const DEFAULT_DRAGON_RAJA_BRUSH_FONT_URL =
+  'https://meimoaiimg.com/user/701473/ma-shan-zheng-dragon-raja.ttf?i=1344575'
 
 const dragonRajaMedia = {
-  welcomePosterUrl: process.env.DRAGON_RAJA_WELCOME_POSTER_URL ?? '',
+  welcomePosterUrl: process.env.DRAGON_RAJA_WELCOME_POSTER_URL ?? DEFAULT_DRAGON_RAJA_WELCOME_POSTER_URL,
   cassellCrestUrl: process.env.DRAGON_RAJA_CASSELL_CREST_URL ?? '',
   storyDesktopUrl: process.env.DRAGON_RAJA_STORY_DESKTOP_URL ?? '',
   storyMobileUrl: process.env.DRAGON_RAJA_STORY_MOBILE_URL ?? '',
@@ -16,7 +20,7 @@ const dragonRajaMedia = {
   localMapUrl: process.env.DRAGON_RAJA_LOCAL_MAP_URL ?? '',
   codexAssetsUrl: process.env.DRAGON_RAJA_CODEX_ASSETS_URL ?? '',
   paperGrainUrl: process.env.DRAGON_RAJA_PAPER_GRAIN_URL ?? '',
-  brushFontUrl: process.env.DRAGON_RAJA_BRUSH_FONT_URL ?? '',
+  brushFontUrl: process.env.DRAGON_RAJA_BRUSH_FONT_URL ?? DEFAULT_DRAGON_RAJA_BRUSH_FONT_URL,
   storySerifFontUrl: process.env.DRAGON_RAJA_SERIF_FONT_URL ?? '',
   storySansFontUrl: process.env.DRAGON_RAJA_SANS_FONT_URL ?? '',
   storyMonoFontUrl: process.env.DRAGON_RAJA_MONO_FONT_URL ?? '',
